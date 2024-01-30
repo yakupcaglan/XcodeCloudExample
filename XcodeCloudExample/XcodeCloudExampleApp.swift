29bc836e0eabcc4eb7603e9d20c6b78e9575d174
Yakup Çağlan <yakup.caglan@hepsiburada.com>
2024-01-30 20:59:44 +0300

    Initial Commit

---
+++ XcodeCloudExample/XcodeCloudExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudExampleApp.swift
+//  XcodeCloudExample
+//
+//  Created by Yakup Çağlan on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
