669c88c4b5726085a1c9d93179758a8cc3589df3
yakupcaglan <yakupcaglan22@gmail.com>
2025-01-13 15:00:23 +0300

    Improves

---
+++ SmokeTests/SmokeTests.swift
@@ -0,0 +1,14 @@
+//
+//  SmokeTests.swift
+//  XcodeCloud_Example
+//
+//  Created by Yakup Caglan on 13.01.2025.
+//
+import Testing
+
+struct SmokeTests {
+    @Test
+    func testExample1() async throws {
+        #expect(10 == 10)
+    }
+}
