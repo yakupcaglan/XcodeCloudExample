5d2defe275311501bf0052af046e266fac93d99a
yakupcaglan <yakupcaglan22@gmail.com>
2025-01-13 16:33:24 +0300

    add bold

--- XcodeCloud_Example/ContentView.swift
+++ XcodeCloud_Example/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))").bold()
                     } label: {
                         Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
